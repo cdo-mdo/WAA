bbfdfbca44f39c8d6867a0a730c1a796a1d90431
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2025-10-16 19:12:39 -0500

    init lab-01

---
+++ lab-01/Lab1ServletCalculator/src/main/java/servlets/CalculatorServlet.java
@@ -0,0 +1,43 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calc")
+public class CalculatorServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //show calculator page
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>SIMPLE CALCULATOR<br><br><br></head>");
+        out.println("<body>");
+        out.println("<form method = 'post' action = 'calc'>");
+        out.println("enter the first number:<br>");
+        out.println("<input type = 'text' name='number1'><br><br>");
+        out.println("enter the second number:<br>");
+        out.println("<input type = 'text' name='number2'><br><br>");
+        out.println("enter the operation:<br><br>");
+        out.println("<input type ='radio' name = 'op' value = '+'>add<br>");
+        out.println("<input type = 'radio' name = 'op' value = '-'>sub<br>");
+        out.println("<input type = 'radio' name = 'op' value = '*'>mul<br>");
+        out.println("<input type = 'radio' name = 'op' value = '/'>div<br><br>");
+        out.println("<input type = 'submit' name = 'result' value = 'submit'><br>");
+        out.println("</body>");
+        out.println("</html>");
+        out.flush();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //show result page
+        PrintWriter out = response.getWriter();
+        out.println("doPost is called");
+        out.flush();
+    }
+
+}
